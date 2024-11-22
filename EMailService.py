cf1d43e80e1f081166ffa2c3d12fc45db5687543
Mephisto2409 <toadbella@gmail.com>
2024-11-22 09:45:24 +0100

    Implemented IMAP sendEmail method
    via SMTP Protocol

--- EMailService.py
+++ EMailService.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from email2 import Email
+from email2 import *
 from imaplib import IMAP4_SSL
+from smtplib import SMTP_SSL, SMTP_SSL_PORT
 
 class ProtocolTemplate(ABC):
     
@@ -30,12 +31,14 @@ class ProtocolTemplate(ABC):
 
 class ImapProtocol(ProtocolTemplate):
     
+    #email address
     user_username = None
+    #or imappassword
     user_passwort = None
     host = "imap.gmail.com"
 
     IMAP = IMAP4_SSL(host)
-    
+    SMTP_HOST = host
 
     @property
     def logged_in(self) -> bool:
@@ -53,8 +56,40 @@ class ImapProtocol(ProtocolTemplate):
         self.user_username = None
         pass
     
-    def sendEmail(self,Email:Email) -> bool:
+    def sendEmail(self, email:Email) -> bool:
         """Requierment: User is logged in"""
+        SMTP_USER = self.user_username
+        SMTP_PASS = self.user_passwort
+
+        to = []
+        cc = []
+        bcc = []
+        for recipent in email.recipients:
+            match (recipent.kind):
+                case RecipientKind.to:
+                    to += [recipent.contact.email_address]
+                case RecipientKind.cc:
+                    cc += [recipent.contact.email_address]
+                case RecipientKind.bcc:
+                    bcc += [recipent.contact.email_address]
+
+        #craft email
+        from_email = SMTP_USER
+        to_emails = to
+        body = email.body
+        headers = f"From: {from_email}\r\n"
+        headers += f"To: {', '.join(to_emails)}\r\n"
+        headers += f"Subject: {email.subject}\r\n"
+        email_message = headers + "\r\n" + body
+
+        #connect/authenticate
+        smtp_server = SMTP_SSL(self.SMTP_HOST, port = SMTP_SSL_PORT)
+        smtp_server.set_debuglevel(1)
+        smtp_server.login(SMTP_USER, SMTP_PASS)
+        smtp_server.sendmail(from_email, to_emails, email_message)
+        
+        #disconnect
+        smtp_server.quit()
         pass
 
     def deleteEmail(self, uid:int) -> bool:
@@ -110,17 +145,28 @@ if __name__ == "__main__":
     imap = ImapProtocol()
     exchange = ExchangeProtocol()
 
+    	
+    test = Email(
+        
+        subject="Hello",
+        body="World",
+        recipients=[EmailReception(contact=(Contact(email_address ="praxisprojekt-remail@uni-due.de")), kind=RecipientKind.to)])
+
     print("IMAP Logged_in: ",imap.logged_in)
     imap.login("thatchmilo35@gmail.com","mgtszvrhgkphxghm")
     print("IMAP Logged_in: ",imap.logged_in)
+
+    imap.sendEmail(test)
+    print("sent?")
+    
     imap.logout()
     print("IMAP Logged_in: ",imap.logged_in)
 
     #exchange
 
-    print("Exchange Logged_in: ",exchange.logged_in)
-    exchange.login("praxisprojekt-remail@uni-due.de","6jTDTk6hS3j^b%@tw")
-    print("Exchange Logged_in: ",exchange.logged_in)
-    exchange.logout()
-    print("Exchange Logged_in: ",exchange.logged_in)
+    #print("Exchange Logged_in: ",exchange.logged_in)
+    #exchange.login("praxisprojekt-remail@uni-due.de","6jTDTk6hS3j^b%@tw")
+    #print("Exchange Logged_in: ",exchange.logged_in)
+    #exchange.logout()
+    #print("Exchange Logged_in: ",exchange.logged_in)
 
