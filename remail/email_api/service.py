f9eac6321ed64a4817573f78d5a05b37863fc8a9
JNAOB <120253002+JNAOB@users.noreply.github.com>
2024-12-13 10:18:23 +0100

    change import to new location object to models

--- remail/email_api/service.py
+++ remail/email_api/service.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from remail.email_api.object import Email, EmailReception, Attachment, Contact, RecipientKind
+from remail.database_api.models import Email, EmailReception, Attachment, Contact, RecipientKind
 from imapclient import IMAPClient
 from smtplib import SMTP_SSL,SMTP_SSL_PORT, SMTPAuthenticationError,SMTPRecipientsRefused,SMTPServerDisconnected,SMTPDataError,SMTPConnectError,SMTPHeloError,SMTPNotSupportedError
 import email
